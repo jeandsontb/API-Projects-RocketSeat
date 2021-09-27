03bd4f5969735d345ff75cdb2403e937f0b51c76
jeandson <jeandsontb@gmail.com>
2021-09-27 16:12:53 -0300

    SOLID Implements

---
+++ src/repositories/PostgresCategoryRepository.ts
@@ -0,0 +1,20 @@
+import { Category } from "../model/Category";
+import {
+  ICategoriesRepositories,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
+
+class PostgresCategoryRepository implements ICategoriesRepositories {
+  findByName(name: string): Category {
+    console.log(name);
+    return null;
+  }
+  list(): Category[] {
+    return null;
+  }
+  create({ name, description }: ICreateCategoryDTO): void {
+    console.log(name, description);
+  }
+}
+
+export { PostgresCategoryRepository };
