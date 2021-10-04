28c824f1c61e406b0f55832450ac2a7cd9926bee
jeandson <jeandsontb@gmail.com>
2021-10-04 16:59:32 -0300

    Add authentications implementations

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUserCase.ts
@@ -0,0 +1,57 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUserCase {
+  constructor(
+    @inject("UserRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or Password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or Password incorrect");
+    }
+
+    const token = sign({}, "7e84ec654d4faa95dcd739a25bb0ae1a", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const returnToken: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+
+    return returnToken;
+  }
+}
+
+export { AuthenticateUserUserCase };
