31a02dad59aecc478922cce708c3497bcb98fe52
jeandson <jeandsontb@gmail.com>
2021-10-06 21:53:39 -0300

    Refactor imports

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
-import { AppError } from "../../../../errors/AppError";
-import { ICategoriesRepositories } from "../../repositories/ICategoriesRepository";
+import { AppError } from "@errors/AppError";
+import { ICategoriesRepositories } from "@modules/cars/repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
--- src/modules/cars/useCases/listCategories/ListCategoryUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoryUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
-import { Category } from "../../entities/Category";
-import { ICategoriesRepositories } from "../../repositories/ICategoriesRepository";
+import { Category } from "@modules/cars/entities/Category";
+import { ICategoriesRepositories } from "@modules/cars/repositories/ICategoriesRepository";
 
 @injectable()
 class ListCategoryUseCase {
