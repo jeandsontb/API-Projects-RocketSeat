b878966f099bf0c698321fe157ce245aa3b9bfa6
jeandson <jeandsontb@gmail.com>
2021-10-11 15:55:26 -0300

    Create cars into in table

---
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,45 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateCarDTO } from "../../../dtos/ICreateCarDTO";
+import { ICarsRepository } from "../../../repositories/ICarsRepository";
+import { Car } from "../entities/Car";
+
+class CarsRepository implements ICarsRepository {
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = getRepository(Car);
+  }
+
+  async create({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    category_id,
+    fine_amount,
+    brand,
+  }: ICreateCarDTO): Promise<Car> {
+    const car = this.repository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      category_id,
+      fine_amount,
+      brand,
+    });
+
+    await this.repository.save(car);
+
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    const car = await this.repository.findOne({ license_plate });
+
+    return car;
+  }
+}
+
+export { CarsRepository };
